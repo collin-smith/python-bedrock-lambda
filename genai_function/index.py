da5addae984a9259085fe701c28e734a0f07de81
Collin Smith <collin.smith@gmail.com>
2024-05-06 20:29:11 -0600

    Initial checkin for the Python Bedrock Lambda

---
+++ genai_function/index.py
@@ -0,0 +1,58 @@
+import json
+import boto3
+
+client = boto3.client('bedrock-runtime')
+
+def handler(event, context):
+
+    body = json.loads(event.get('body', '{}'))
+    #setting defult prompt if none provided
+    prompt = body.get('prompt', 'Write a text to be posted on my social media channels about how Amazon Bedrock works')
+
+
+    model_id = "anthropic.claude-3-opus-20240229-v1:0"
+
+    response = client.invoke_model(
+                modelId=model_id,
+                body=json.dumps(
+{
+    "anthropic_version": "bedrock-2023-05-31",
+    "max_tokens": 1000,
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+
+          {
+            "type": "text",
+            "text": prompt
+          }
+        ]
+      }
+    ]
+  }
+
+                ),
+            )
+
+            # Process and print the response
+    result = json.loads(response.get("body").read())
+    input_tokens = result["usage"]["input_tokens"]
+    output_tokens = result["usage"]["output_tokens"]
+    output_list = result.get("content", [])
+
+    print("Invocation details:")
+    print(f"- The input length is {input_tokens} tokens.")
+    print(f"- The output length is {output_tokens} tokens.")
+
+    print(f"- The model returned {len(output_list)} response(s):")
+    for output in output_list:
+                print(output["text"])
+
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'generated-text': result
+        })
+    }
--- python_bedrock_lambda/python_bedrock_lambda_stack.py
+++ python_bedrock_lambda/python_bedrock_lambda_stack.py
@@ -1,7 +1,10 @@
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+        aws_iam as iam,
+        Duration,
+    aws_apigateway as apigw
 )
 from constructs import Construct
 
@@ -10,10 +13,38 @@ class PythonBedrockLambdaStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PythonBedrockLambdaQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        model_id = "anthropic.claude-3-opus-20240229-v1:0"
+        #foundation-model/anthropic.claude-v2
+        #foundation-model/anthropic.claude-3-sonnet-20240229-v1:0
+        #foundation-model/anthropic.claude-3-opus-20240229-v1:0
+
+
+               #add policy to invoke bedrock model
+        invoke_model_policy = iam.Policy(self, "InvokeModelPolicy",
+            statements=[
+                iam.PolicyStatement(
+                    actions=["bedrock:InvokeModel"],
+                    resources=[f"arn:aws:bedrock:{self.region}::foundation-model/anthropic.claude-3-opus-20240229-v1:0"]
+                )
+            ]
+        )
+
+        # Create the Lambda function and attach the layer
+        lambda_function = _lambda.Function(self, "MyFunction",
+            runtime=_lambda.Runtime.PYTHON_3_10,
+            handler="index.handler",
+            code=_lambda.Code.from_asset("./genai_function"),
+           # layers=[layer],
+            timeout=Duration.seconds(30)
+            )
+
+        invoke_model_policy.attach_to_role(lambda_function.role)
+
+
+         #create api gateway
+        api = apigw.RestApi(self, "BedrockApi",)
+
+        #create a new resource
+        text_gen_resource = api.root.add_resource("genai")
+        text_gen_resource.add_method("POST", apigw.LambdaIntegration(lambda_function))
